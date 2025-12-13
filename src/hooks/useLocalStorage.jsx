cfd6bc112e3b7b3b54d039ab79c326824dcfa20a
emretantu <emretantu@gmail.com>
2025-12-14 01:54:41 +0300

    Implement useLocalStorage hook

---
+++ src/hooks/useLocalStorage.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react"
+
+export const useLocalStorage = (key, initialValue) => {
+  
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      if (typeof window === "undefined") {
+        return initialValue;
+      }
+      const item = localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue
+    } catch (error) {
+      console.log(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      if (typeof window !== "undefined") {
+        localStorage.setItem(key, JSON.stringify(valueToStore));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    const handleStorageChange = (e) => {
+      if (e.key === key && e.newValue) {
+        setStoredValue(JSON.parse(e.newValue));
+      }
+    }
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      window.removeEventListener("storage", handleStorageChange);
+    }
+  }, [key]);
+
+  return [storedValue, setValue];
+
+}
