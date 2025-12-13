87cb1e66c02097d0cadfa920a4a6d3cba5da9b09
emretantu <emretantu@gmail.com>
2025-12-14 02:27:50 +0300

    Implement ThemeSwitcher component

--- src/components/ThemeSwitcher/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.jsx
@@ -1,8 +1,19 @@
 import styles from "./ThemeSwitcher.module.css"
+import moonIcon from "../../assets/icons/icon-moon.svg";
+import sunIcon from "../../assets/icons/icon-sun.svg";
+import { useTheme } from "../../context/ThemeContext";
 
 const ThemeSwitcher = () => {
+
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div>ThemeSwitcher</div>
+    <button
+      className={styles.themeSwitcher}
+      onClick={() => toggleTheme()}
+    >
+      <img src={theme === "light" ? moonIcon : sunIcon} />
+    </button>
   )
 }
 
