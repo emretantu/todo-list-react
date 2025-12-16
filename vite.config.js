116aa451dd36ff21cfd8197c6e89f509f7c53367
emretantu <emretantu@gmail.com>
2025-12-16 23:40:40 +0300

    Add favicon

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "todo-list-react"
+export default defineConfig(({ command }) => {
+  const isProd = command === 'build'
+
+  return {
+    plugins: [react()],
+    base: isProd ? "/todo-list-react/" : "/",
+  }
 })
