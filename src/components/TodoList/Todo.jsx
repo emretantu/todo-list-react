f3f81cbe7cb5768a016ea973c69516ff35874a01
emretantu <emretantu@gmail.com>
2025-12-19 00:31:25 +0300

    Refactor todo list divider for drag and drop

--- src/components/TodoList/Todo.jsx
+++ src/components/TodoList/Todo.jsx
@@ -8,25 +8,27 @@ const Todo = ({ id, todoText, isDone, onCheckClick, onDeleteClick }) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <>
-      <li className={styles.todoItem}>
-        <div 
-          className={styles.todo}
-          onClick={() => onCheckClick(id)}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-        >
-          <StylishCheckbox
-            isDone={isDone}
-            isHovered={isHovered}
-          />
-          <input type="checkbox" name={"checkbox_"+id} id={"checkbox_"+id} checked={isDone} readOnly />
-          <label className={`${styles.todoText} ${isDone ? styles.done : ""}`} htmlFor={"checkbox_"+id} onClick={(e) => e.preventDefault()}>{todoText}</label>
-        </div>
-        <img className={styles.crossIcon} src={CrossIcon} alt="Cross Icon" onClick={() => onDeleteClick(id)} />
-      </li>
-      <hr className={styles.divider} />
-    </>
+    <li className={styles.todoItem}>
+
+      <div 
+        className={styles.todo}
+        onClick={() => onCheckClick(id)}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+
+        <StylishCheckbox
+          isDone={isDone}
+          isHovered={isHovered}
+        />
+        <input type="checkbox" name={"checkbox_"+id} id={"checkbox_"+id} checked={isDone} readOnly />
+        <label className={`${styles.todoText} ${isDone ? styles.done : ""}`} htmlFor={"checkbox_"+id} onClick={(e) => e.preventDefault()}>{todoText}</label>
+
+      </div>
+
+      <img className={styles.crossIcon} src={CrossIcon} alt="Cross Icon" onClick={() => onDeleteClick(id)} />
+
+    </li>
   )
 }
 
